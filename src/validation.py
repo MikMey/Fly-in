f92ce7fa4a812fba2209e21543f740214d7f8d2f
Mika Meyer <mimeyer@c2r12s9.42wolfsburg.de>
2026-09-02 15:36:30 +0200

    feat: validation complete

--- src/validation.py
+++ src/validation.py
@@ -63,6 +63,12 @@ class Connection(BaseModel):
 			self.pop('metadata')
 		return self
 
+	@model_validator(mode='after')
+	def validate(self):
+		if self.start == self.end:
+			sys.exit(f"Connection \"{self.start}-{self.end}\" ends in itself")
+		return self
+
 
 drones_pattern = re.compile(
 	r"^(?P<group>\w+):\s+(?P<number>\w+)$"
@@ -81,7 +87,7 @@ metadata_pattern = re.compile(
 
 GROUPS: list = ["nb_drones", "start_hub", "end_hub", "hub", "connection"]
 PATTERNS: list = [drones_pattern, hub_pattern, connection_pattern]
-MATCHES: dict[str|BaseModel] = {
+MATCHES: dict[str, BaseModel] = {
 	'nb_drones': Drone,
 	'start_hub': Hub,
 	'end_hub': Hub,
@@ -89,19 +95,25 @@ MATCHES: dict[str|BaseModel] = {
 	'connection': Connection
 }
 
-def _create_obj(class_type: BaseModel, objects: list[Drone|Hub|Connection], matched: re.Match) -> list[Drone|Hub|Connection]:
+def _create_obj(class_type: BaseModel, objects: dict[Drone|Hub|Connection, list[Drone|Hub|Connection]], matched: re.Match)\
+	-> dict[Drone|Hub|Connection, list[Drone|Hub|Connection]]:
 	checked: Drone|Hub|Connection = class_type.model_validate(matched.groupdict())
 	if type(checked) == Hub:
-		if any(checked.name == i.name for i in objects if type(i) == Hub):
+		if any(checked.name == i.name for i in objects[type(checked)] if type(i) == Hub):
 			sys.exit("Duplicate name")
 	elif type(checked) == Connection:
-		if any(checked.start == i.start for i in objects if type(i) == Connection) and any(checked.end == i.end for i in objects if type(i) == Connection):
+		if any(checked.start == i.start for i in objects[type(checked)] if type(i) == Connection)\
+			and any(checked.end == i.end for i in objects[type(checked)] if type(i) == Connection):
 			sys.exit("Duplicate route")
-	objects.append(checked)
+	objects[type(checked)].append(checked)
 	return(objects)
 
-def cache_input(filepath: str) -> list[Drone|Hub|Connection]:
-	objects: list[Drone|Hub|Connection] = []
+def cache_input(filepath: str) -> dict[Drone|Hub|Connection, list[Drone|Hub|Connection]]:
+	objects: dict[Drone|Hub|Connection, list[Drone|Hub|Connection]] = {
+		Drone: [],
+		Hub: [],
+		Connection: []
+	}
 	with open(file=filepath, mode='r') as file:
 		for i, line  in enumerate(file):
 			if "#" in line or ":" not in line:
@@ -122,9 +134,43 @@ def cache_input(filepath: str) -> list[Drone|Hub|Connection]:
 				sys.exit(f"Error in validation:\n{err}")
 			except Exception as err:
 				sys.exit(f"Error:\n{err}")
-	for i in objects:
-		print(type(i), i)
+	objects = verify(objects)
+	for value in objects.values():
+		for i in value:
+			print(i)
 	return(objects)
 
-def verify():
-	pass
+def _clear_connections(objects: dict[Drone|Hub|Connection, list[Drone|Hub|Connection]]) -> dict[Drone|Hub|Connection, list[Drone|Hub|Connection]]:
+	for con in objects[Connection]:
+		if not any(con.start == hub.name for hub in objects[Hub]) or\
+			not any(con.end == hub.name for hub in objects[Hub]):
+			objects[Connection].remove(con)
+	return objects
+
+def _clear_hubs(objects: dict[Drone|Hub|Connection, list[Drone|Hub|Connection]]) -> dict[Drone|Hub|Connection, list[Drone|Hub|Connection]]:
+	i = 0
+	remove = True
+	while remove:
+		remove = False
+		for hub in objects[Hub]:
+			if hub.group == 'hub':
+				for con in objects[Connection]:
+					if hub.name == con.start or hub.name == con.end:
+						i += 1
+				if i <= 1 or hub.zone == 'blocked':
+					objects[Hub].remove(hub)
+					objects = _clear_connections(objects)
+					remove = True
+			i = 0
+	return objects
+
+def verify(objects: dict[Drone|Hub|Connection, list[Drone|Hub|Connection]]) -> dict[Drone|Hub|Connection, list[Drone|Hub|Connection]]:
+	for con in objects[Connection]:
+		if any(con.start == hub.name for hub in objects[Hub]) and\
+			any(con.end == hub.name for hub in objects[Hub]):
+			break
+		else:
+			sys.exit(f"Connection \"{con.start}-{con.end}\"contains invalid nodes")
+	objects = _clear_hubs(objects)
+
+	return objects
