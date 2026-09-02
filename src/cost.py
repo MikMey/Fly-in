d394abc84f8a2eadb4613dde8fcd6ef90a0b6fb4
Mika Meyer <mimeyer@c2r12s9.42wolfsburg.de>
2026-09-02 20:45:53 +0200

    completing basic cost calculation on bfs

---
+++ src/cost.py
@@ -0,0 +1,25 @@
+import sys
+from typing import Any
+
+from .models import Data, Hub, Connection, Drone
+
+
+def cost_calc(objects: Data) -> Data:
+	#minor bug, connection cost always from smaller node to greater node
+	stack: list[Hub] = []
+	i = 0
+	stack.append(next(elem for elem in objects[Hub] if elem.group =='end_hub'))
+	stack[i].max_cost = 0
+	while i < (len(objects[Hub]) - 1):
+		for con in stack[i].connections:
+			if con.max_cost == -1 and (con.end == stack[i].name or con.start == stack[i].name):
+				con.max_cost = stack[i].max_cost + 1
+			if con.start == stack[i].name:
+				check = next(elem for elem in objects[Hub] if elem.name == con.end)
+			else:
+				check = next(elem for elem in objects[Hub] if elem.name == con.start)
+			if check.max_cost == -1:
+				check.max_cost = con.max_cost
+				stack.append(check)
+		i += 1
+	return objects
