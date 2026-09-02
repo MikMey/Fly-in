8d39fc31d980dff0f8a484f12eebc56095cd72a4
Mika Meyer <mimeyer@c2r12s9.42wolfsburg.de>
2026-09-02 20:44:57 +0200

    feat: completes validation and verification

--- src/validation.py
+++ src/validation.py
@@ -5,85 +5,20 @@ import re
 from pydantic import BaseModel, Field, ValidationError, model_validator
 from colour import Color
 
-
-
-class Drone(BaseModel):
-	group: str = "nb_drones"
-	number: int = Field(..., ge=1, le=999999)
-
-ZONES: list = ['normal', 'blocked', 'priority', 'restricted']
-
-class Hub(BaseModel):
-	group: str = Field(..., min_length=3, max_length=9)
-	name: str = Field(..., min_length=1)
-	x: int = Field(..., ge=0, le=9999)
-	y: int = Field(..., ge=0, le=9999)
-	zone: str = Field(default="normal")
-	color: Any = Field(default=None)
-	max_drones: int = Field(default=1, ge=0, le=99999)
-
-	@model_validator(mode="before")
-	def prep(self: dict[str, str | Any]):
-		if self['metadata']:
-			self['metadata'] = self['metadata'].split(' ')
-			for i in self['metadata']:
-				if not re.match(metadata_pattern, i):
-					sys.exit(f"Invalid Metadata format:\n\"{i}\"")
-				metadata = i.split('=', 1)
-				self[metadata[0]] = metadata[1]
-			self.pop('metadata')
-		if self['color']:
-			try:
-				self['color'] = Color(self['color'])
-			except Exception as err:
-				sys.exit(f"Color Error in \"{self['name']}\":\n{err}")
-		return self
-
-	@model_validator(mode='after')
-	def validate(self):
-		if self.zone not in ZONES:
-			sys.exit(f"Invalid hub zone \"{self.zone}\" at \"{self.name}\"")
-		return self
-
-class Connection(BaseModel):
-	group: str = "connection"
-	start: str = Field(..., min_length=1)
-	end: str = Field(..., min_length=1)
-	max_link_capacity: int = Field(default=1, ge=0, le=99999)
-
-	@model_validator(mode="before")
-	def prep(self: dict[str, str | Any]):
-		if self['metadata']:
-			self['metadata'] = self['metadata'].split(' ')
-			for i in self['metadata']:
-				if not re.match(metadata_pattern, i):
-					sys.exit(f"Invalid Metadata format:\n\"{i}\"")
-				metadata = i.split('=', 1)
-				self[metadata[0]] = metadata[1]
-			self.pop('metadata')
-		return self
-
-	@model_validator(mode='after')
-	def validate(self):
-		if self.start == self.end:
-			sys.exit(f"Connection \"{self.start}-{self.end}\" ends in itself")
-		return self
-
+from .models import Drone, Connection, Hub, Data
 
 drones_pattern = re.compile(
 	r"^(?P<group>\w+):\s+(?P<number>\w+)$"
 )
 hub_pattern = re.compile(
-    r"^(?P<group>\w+):\s+(?P<name>\w+)\s+(?P<x>\d+)\s+(?P<y>\d+)" \
+    r"^(?P<group>\w+):\s+(?P<name>\w+)\s+(?P<x>[^ ]+)\s+(?P<y>[^ ]+)" \
     r"(?:\s+\[(?P<metadata>[^\]]+)\])?$"
 )
 connection_pattern = re.compile(
 	r"^(?P<group>\w+):\s+(?P<start>\w+)-(?P<end>\w+)" \
 	r"(?:\s+\[(?P<metadata>[^\]]+)\])?$"
 )
-metadata_pattern = re.compile(
-	r"^(\w+)=([^\s]+)$"
-)
+
 
 GROUPS: list = ["nb_drones", "start_hub", "end_hub", "hub", "connection"]
 PATTERNS: list = [drones_pattern, hub_pattern, connection_pattern]
@@ -95,8 +30,8 @@ MATCHES: dict[str, BaseModel] = {
 	'connection': Connection
 }
 
-def _create_obj(class_type: BaseModel, objects: dict[Drone|Hub|Connection, list[Drone|Hub|Connection]], matched: re.Match)\
-	-> dict[Drone|Hub|Connection, list[Drone|Hub|Connection]]:
+def _create_obj(class_type: BaseModel, objects: Data, matched: re.Match)\
+	-> Data:
 	checked: Drone|Hub|Connection = class_type.model_validate(matched.groupdict())
 	if type(checked) == Hub:
 		if any(checked.name == i.name for i in objects[type(checked)] if type(i) == Hub):
@@ -108,8 +43,8 @@ def _create_obj(class_type: BaseModel, objects: dict[Drone|Hub|Connection, list[
 	objects[type(checked)].append(checked)
 	return(objects)
 
-def cache_input(filepath: str) -> dict[Drone|Hub|Connection, list[Drone|Hub|Connection]]:
-	objects: dict[Drone|Hub|Connection, list[Drone|Hub|Connection]] = {
+def cache_input(filepath: str) -> Data:
+	objects: Data = {
 		Drone: [],
 		Hub: [],
 		Connection: []
@@ -135,19 +70,17 @@ def cache_input(filepath: str) -> dict[Drone|Hub|Connection, list[Drone|Hub|Conn
 			except Exception as err:
 				sys.exit(f"Error:\n{err}")
 	objects = verify(objects)
-	for value in objects.values():
-		for i in value:
-			print(i)
+	
 	return(objects)
 
-def _clear_connections(objects: dict[Drone|Hub|Connection, list[Drone|Hub|Connection]]) -> dict[Drone|Hub|Connection, list[Drone|Hub|Connection]]:
+def _clear_connections(objects: Data) -> Data:
 	for con in objects[Connection]:
 		if not any(con.start == hub.name for hub in objects[Hub]) or\
 			not any(con.end == hub.name for hub in objects[Hub]):
 			objects[Connection].remove(con)
 	return objects
 
-def _clear_hubs(objects: dict[Drone|Hub|Connection, list[Drone|Hub|Connection]]) -> dict[Drone|Hub|Connection, list[Drone|Hub|Connection]]:
+def _clear_hubs(objects: Data) -> Data:
 	i = 0
 	remove = True
 	while remove:
@@ -164,7 +97,17 @@ def _clear_hubs(objects: dict[Drone|Hub|Connection, list[Drone|Hub|Connection]])
 			i = 0
 	return objects
 
-def verify(objects: dict[Drone|Hub|Connection, list[Drone|Hub|Connection]]) -> dict[Drone|Hub|Connection, list[Drone|Hub|Connection]]:
+def _attach_connections(objects: Data) -> Data:
+	for hub in objects[Hub]:
+		for con in objects[Connection]:
+			if hub.name == con.start or hub.name == con.end:
+				hub.connections.append(con)
+	return objects
+
+def verify(objects: Data) -> Data:
+	if not any('start_hub' == hub.group for hub in objects[Hub]) or\
+		not any('end_hub' == hub.group for hub in objects[Hub]):
+		sys.exit(f"Start and/or exit dont exit")
 	for con in objects[Connection]:
 		if any(con.start == hub.name for hub in objects[Hub]) and\
 			any(con.end == hub.name for hub in objects[Hub]):
@@ -172,5 +115,6 @@ def verify(objects: dict[Drone|Hub|Connection, list[Drone|Hub|Connection]]) -> d
 		else:
 			sys.exit(f"Connection \"{con.start}-{con.end}\"contains invalid nodes")
 	objects = _clear_hubs(objects)
+	objects = _attach_connections(objects)
 
 	return objects
