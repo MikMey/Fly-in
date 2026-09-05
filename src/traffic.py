9803f6d54d7446715030f16afc70e1610f7043c5
Mika Meyer <mimeyer@c2r7s6.42wolfsburg.de>
2026-09-05 22:37:41 +0200

    feat: adds coloring; fix: correctly updates path usage; fix: correctly limits paths based on current drone amount

--- src/traffic.py
+++ src/traffic.py
@@ -2,6 +2,8 @@ import sys
 from time import sleep
 from typing import Any
 
+from colour import Color
+
 from .models import Data, Hub, Connection, Drone
 
 def _find_connection(hub: Hub, moving: int) -> Hub:
@@ -11,16 +13,32 @@ def _find_connection(hub: Hub, moving: int) -> Hub:
 			continue
 		targets.append(next(target for target in connection.hubs if target.name != hub.name))
 	for target in targets.copy():
-		if len(target.drones) >= target.max_drones:
+		if len(target.drones) >= target.max_drones and target.group != 'end_hub':
+			targets.remove(target)
+		elif target.max_cost + 1 > hub.max_cost + moving:
 			targets.remove(target)
 	if not targets or not targets[0]:
 		return None
 	targets.sort(key=lambda target: target.max_cost)
-	return targets[0]
+	cost = targets[0].max_cost
+	for item in targets.copy():
+		if item.max_cost > cost:
+			targets.remove(item)
+	target = targets[0]
+	for item in targets:
+		if item.zone == "priority":
+			target = item
+			break
+	con = next(con for con in target.connections if hub.name in con.link)
+	con.used += 1
+	return target
 	
 
 def _show_move(target: Hub, drone: str, restricted: bool = False) -> None:
-	print(f"{drone}-{target.name}", end="")
+	r = int(target.color.red * 255)
+	g = int(target.color.green * 255)
+	b = int(target.color.blue * 255)
+	print(f"\033[38;2;{r};{g};{b}m{drone}-{target.name}\033[0m", end="")
 	if restricted:
 		print("[restricted]", end=" ")
 	else:
@@ -44,7 +62,8 @@ def send_drones(objects: Data) -> int:
 	while number of drones > drones at end hub
 	"""
 	while objects[Drone][0].number > len(objects[Hub][0].drones):
-		# sleep(1)
+		sleep(0.1)
+		busy: list[str] = []
 		for con in objects[Connection]:
 			con.used = 0
 		turn_counter += 1
@@ -55,15 +74,15 @@ def send_drones(objects: Data) -> int:
 			if len(hub.drones) == 0:
 				continue
 			for drone, counter in list(hub.drones.items()):
+				if drone in busy:
+					continue
+				busy.append(drone)
 				if counter > 0:
 					hub.drones[drone] = 0
 					continue
 				target: Hub = _find_connection(hub, moving)
 				if target == None:
 					continue
-				#TODO hubs called after current, dont know if drone was sent here this turn
-				#TODO cost implementation to _find_connection
-				#TODO drones dont reach end
 				hub.drones.__delitem__(drone)
 				target.drones[drone] = 0
 				if target.zone == 'restricted':
@@ -71,11 +90,9 @@ def send_drones(objects: Data) -> int:
 					_show_move(target, drone, True)
 				else:
 					_show_move(target, drone)
-				if target.group == 'end_zone':
+				if target.group == 'end_hub':
 					moving -= 1
-		if turn_counter > 50:
-			break
-		print(f"\n{moving}", end="\n")
+		print(f"", end="\n")
 
 	return turn_counter
 
